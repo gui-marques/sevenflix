a82e7c0ca228598b9bbfc7c753496f4917cc59a9
Guilherme Silva <guilherme.silva@evolutioninformatica.com.br>
2020-07-28 15:42:13 -0300

    Setup Inicial do Projeto

---
+++ seven-flix/src/Componentes/Menu/Components/ButtonLink/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Button(props) {
+
+return ( 
+    <a href= {props.href} className={props.className}>
+        NovoVideo
+    </a>
+);
+}
+export default Button;
