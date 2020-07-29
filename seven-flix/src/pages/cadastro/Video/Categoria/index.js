b3bfaf8ae7ef99adf5cbad498bea69a9601111e6
Guilherme Silva <guilherme.silva@evolutioninformatica.com.br>
2020-07-29 18:27:31 -0300

    voltando

--- seven-flix/src/pages/cadastro/Video/Categoria/index.js
+++ seven-flix/src/pages/cadastro/Video/Categoria/index.js
@@ -2,95 +2,49 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import PageDefoult from '../../../../PageDefault';
 
-
-
-
 function CadastroCategoria() {
-  const valoresIniciais = {
-    nome: '',
-    descricao: '',
-    cor: '',
-  }
 
-  const [values, setValues] = useState(valoresIniciais);
-  
-  const [categorias, setCategorias] = useState([]);
-
-  function setValue(chave, valor) {
-    setValues({
-      ...values,
-      [chave]: valor,
-    });
-  }
-
-  function handleChange(infosDoEvento) {
-    const {getAttribute, value} = infosDoEvento.target; 
-    setValue(
-    getAttribute('name'),
-    value
-    );
-  }
+  const [categorias, setCategorias] = useState(['teste']);
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes')
 
     return (
       <PageDefoult>
-        <h1>Cadastro de Categoria: {values.nome}</h1> 
+        <h1>Cadastro de Categoria: {nomeDaCategoria}</h1> 
   
-        <form onSubmit={function handleSubmit(infosDoEvento) {
-          infosDoEvento.preventDefault();
+        <form onSubmit={function handleSubmit(infoDoEvento) {
+          infoDoEvento.preventDefault();
           setCategorias([
           ...categorias,
-          values
-        ]);      
-
-        setValues(valoresIniciais)  
-
+          nomeDaCategoria]);        
       }}>
-        <div>
+  
           <label>
             Nome da Categoria:
-            <input 
-              type="text"
-              name="nome"
-              value= {values.nome}
-              onChange={handleChange}              
-              />
-          </label>
-          <label>
-            Descrição:
-            <textarea
-              type="text"
-              value= {values.descricao}
-              name="descricao"
-              onChange={handleChange}             
-          />
-          </label>      
-          </div>
-          <div>
-          <label>
-            Cor:
             <input
-              type="color"
-              value= {values.cor}
-              name="cor"
-              onChange={handleChange}
+              type="text"
+              value= {nomeDaCategoria}
+              onChange={function lerdados(infoDoEvento){
+                setNomeDaCategoria(infoDoEvento.target.value);
+
+              }}
             />
           </label>
-          </div>
+  
           <button>
             Cadastrar
           </button>
         </form>
-        
+        <div>
          <ul>
-            {categorias.map((categoria, indice) => {
-              return (
+            {categorias.map((categoria,indice) =>{
+              return(
                 <li key={`&{categoria}${indice}`}>
-                  {categoria.nome}
+                  {categoria}
                   </li>
               )
             })}
           </ul>
-       
+          </div>
         <Link to="/">
           Ir para home
         </Link>
