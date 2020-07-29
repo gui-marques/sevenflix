4bb22781165789629539ae19ff36cfaaefb73a6c
Guilherme Silva <guilherme.silva@evolutioninformatica.com.br>
2020-07-29 13:16:32 -0300

    Aleração

--- seven-flix/src/pages/cadastro/Video/Categoria/index.js
+++ seven-flix/src/pages/cadastro/Video/Categoria/index.js
@@ -2,18 +2,30 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import PageDefoult from '../../../../PageDefault';
 
-function CadastroCategoria(){
-    return(
-
-        <PageDefoult>
-            <h2>Ir para Home</h2>
-            <Link to="/">
-                Ir para Home
-            </Link>
-        </PageDefoult>
-
-
-    );
-}
-
+function CadastroCategoria() {
+    return (
+      <PageDefoult>
+        <h1>Cadastro de Categoria</h1>
+  
+        <form>
+  
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+            />
+          </label>
+  
+          <button>
+            Cadastrar
+          </button>
+        </form>
+  
+  
+        <Link to="/">
+          Ir para home
+        </Link>
+      </PageDefoult>
+    )
+  }
 export default CadastroCategoria;
