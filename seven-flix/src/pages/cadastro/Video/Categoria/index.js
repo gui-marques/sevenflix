e9a080933d3dc165c752034be3ee06b82ee589e8
Guilherme Silva <guilherme.silva@evolutioninformatica.com.br>
2020-08-01 15:08:34 -0300

    Aula 4 finalizada

--- seven-flix/src/pages/cadastro/Video/Categoria/index.js
+++ seven-flix/src/pages/cadastro/Video/Categoria/index.js
@@ -32,7 +32,9 @@ function CadastroCategoria() {
 
     useEffect(() => {
     
-      const URL_TOP = 'http://localhost:8080/categorias';
+      const URL_TOP = window.location.hostname.includes('localhost')
+      ? 'http://localhost:3000/categorias'
+      : 'https://seven-flix.herokuapp.com/categorias';
       fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
